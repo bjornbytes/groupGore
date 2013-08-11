b177e2738d1b9a5afb61fe6597f0d9f3f4cc0deb
tie372 <tie.372@gmail.com>
2013-08-11 16:01:07 -0600

    Shotgun deals damage;

--- data/spell/shotgun.lua
+++ data/spell/shotgun.lua
@@ -7,10 +7,34 @@ Shotgun.activate = function(self)
   self.hp = Shotgun.hp
   self.x = self.owner.x
   self.y = self.owner.y
+  self.len = 700
   self.bullets = {}
+  
   for i = self.owner.angle - math.rad(6), self.owner.angle + math.rad(6), math.rad(3) do
+    local ang, len = i + (math.pi / 2), self.len
+    
+    local endx, endy = self.x + math.cos(ang) * len, self.y + math.sin(ang) * len
+    local targets = {}
+    Players:with(function(p)
+      return p.team ~= self.owner.team and math.hloca(self.x, self.y, endx, endy, p.x, p.y, p.size)
+    end, function(p)
+      table.insert(targets, {
+        id = p.id,
+        dist = math.distance(p.x, p.y, self.x, self.y)
+      })
+    end)
+    
+    table.sort(targets, function(a, b) return a.dist < b.dist end)
+    if targets[1] then
+      print('I HIT PLAYER NUMBER ' .. targets[1].id)
+      local p = Players:get(targets[1].id)
+      len = math.distance(self.x, self.y, p.x, p.y)
+      p:hurt(30)
+    end
+    
     table.insert(self.bullets, {
-      angle = i + (math.pi / 2)
+      angle = i + (math.pi / 2),
+      len = len
     })
   end
 end
@@ -22,7 +46,7 @@ end
 Shotgun.draw = function(self)
   love.graphics.setColor(255, 255, 255, (self.hp / Shotgun.hp) * 255)
   for _, bullet in pairs(self.bullets) do
-    love.graphics.line(self.x, self.y, self.x + math.cos(bullet.angle) * 500, self.y + math.sin(bullet.angle) * 500)
+    love.graphics.line(self.x, self.y, self.x + math.cos(bullet.angle) * bullet.len, self.y + math.sin(bullet.angle) * bullet.len)
   end
   love.graphics.setColor(255, 255, 255, 255)
 end
