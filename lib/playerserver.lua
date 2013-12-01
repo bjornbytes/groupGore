a0ef0edbe347baadfc35985ba893b20f9f00038d
tie372 <tie.372@gmail.com>
2013-11-30 18:53:13 -0700

    Bugfix in respawn;

--- lib/playerserver.lua
+++ lib/playerserver.lua
@@ -57,7 +57,7 @@ function PlayerServer:update()
 end
 
 function PlayerServer:time()
-  self.ded = timer.rot(self.ded, function() Net:emit(evtSpawn, {id = self.id}) end)
+  self.ded = timer.rot(self.ded, function() ovw.net:emit(evtSpawn, {id = self.id}) end)
   if self.ded == 0 then self.ded = false end
 end
 
