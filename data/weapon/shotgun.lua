e20d3d000ddf03b4aaf51dbb10724417ae7ec0c3
bjorn <tie.372@gmail.com>
2014-05-22 22:45:44 +0000

    Fix shotgun crosshair;

--- data/weapon/shotgun.lua
+++ data/weapon/shotgun.lua
@@ -34,8 +34,18 @@ function Shotgun:crosshair()
   local vx, vy = ctx.view:mouseX(), ctx.view:mouseY()
   local d = math.distance(p.x, p.y, vx, vy)
   local alpha = 50 + ((300 - math.clamp(d - 200, 0, 300)) / 300) * 205
+  
+  local dir = p.angle
+  local dx, dy = p.class.handx, p.class.handy
+  x = x + math.dx(dx, dir) - math.dy(dy, dir)
+  y = y + math.dy(dx, dir) + math.dx(dy, dir)
+  
+  dx, dy = self.tipx, self.tipy
+  x = x + math.dx(dx, dir) - math.dy(dy, dir)
+  y = y + math.dy(dx, dir) + math.dx(dy, dir)
+  
   g.setColor(255, 255, 255, alpha)
-  g.circle('line', x, y, math.abs(math.atan(self.spread) / 2) * math.distance(p.x, p.y, vx, vy))
+  g.circle('line', x, y, math.abs(math.atan(self.spread / 2)) * math.distance(p.x, p.y, vx, vy))
 end
 
 return Shotgun
