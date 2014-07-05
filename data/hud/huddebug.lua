0e0860e375e12046ea371f8cedcfbc1fb1ca88d0
bjorn <tie.372@gmail.com>
2014-07-04 23:20:31 -0600

    HudDebug hitboxes in local environment only;

--- data/hud/huddebug.lua
+++ data/hud/huddebug.lua
@@ -3,20 +3,22 @@ HudDebug = class()
 local g = love.graphics
 
 function HudDebug:draw()
-  ctx.players:each(function(p)
-    g.push()
-    g.scale(ctx.view.scale)
-    g.translate(-ctx.view.x, -ctx.view.y)
-    if p.team == purple then g.setColor(190, 160, 220, p.alpha * 100)
-    else g.setColor(240, 160, 140, p.alpha * 100) end
-    p.shape:draw('fill')
-    g.setLineWidth(2)
-    if p.team == purple then g.setColor(190, 160, 220, p.alpha * 255)
-    else g.setColor(240, 160, 140, p.alpha * 255) end
-    p.shape:draw('line')
-    g.setLineWidth(1)
-    g.pop()
-  end)
+  if env ~= 'release' then
+    ctx.players:each(function(p)
+      g.push()
+      g.scale(ctx.view.scale)
+      g.translate(-ctx.view.x, -ctx.view.y)
+      if p.team == purple then g.setColor(190, 160, 220, p.alpha * 100)
+      else g.setColor(240, 160, 140, p.alpha * 100) end
+      p.shape:draw('fill')
+      g.setLineWidth(2)
+      if p.team == purple then g.setColor(190, 160, 220, p.alpha * 255)
+      else g.setColor(240, 160, 140, p.alpha * 255) end
+      p.shape:draw('line')
+      g.setLineWidth(1)
+      g.pop()
+    end)
+  end
 
   g.setColor(255, 255, 255, 100)
   local debug = love.timer.getFPS() .. 'fps'
