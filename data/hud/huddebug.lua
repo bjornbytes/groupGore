1290a0478ceaace8350e3f7eb9aec933262b1566
bjorn <tie.372@gmail.com>
2014-08-14 23:34:56 -0700

    HUD debug shows rates instead of totals;

--- data/hud/huddebug.lua
+++ data/hud/huddebug.lua
@@ -20,12 +20,14 @@ function HudDebug:draw()
     end)
   end
 
+  self.tStart = self.tStart or tick
+
   g.setColor(255, 255, 255, 100)
   local debug = love.timer.getFPS() .. 'fps'
   if ctx.net.server then
     debug = debug .. ', ' .. ctx.net.server:round_trip_time() .. 'ms'
-    debug = debug .. ', ' .. math.floor(ctx.net.host:total_sent_data() / 1000 + .5) .. 'tx'
-    debug = debug .. ', ' .. math.floor(ctx.net.host:total_received_data() / 1000 + .5) .. 'rx'
+    debug = debug .. ', ' .. math.round(ctx.net.host:total_sent_data() / 1000 / ((tick - self.tStart) * tickRate)) .. ' KB/s up'
+    debug = debug .. ', ' .. math.round(ctx.net.host:total_received_data() / 1000 / ((tick - self.tStart) * tickRate)) .. ' KB/s dn'
   end
   g.setFont('aeromatics', ctx.hud.v * .02)
   g.print(debug, ctx.hud.u - g.getFont():getWidth(debug), ctx.hud.v - g.getFont():getHeight())
