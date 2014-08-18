41d90b84fe32aa00ca32792e9fa53a3342803eeb
bjorn <tie.372@gmail.com>
2014-08-17 22:21:34 -0600

    Gooey for meny background;

---
+++ data/gooey/menu/background.lua
@@ -0,0 +1,19 @@
+return {
+  code = 'background',
+  { name = 'background',
+    kind = 'Element',
+    properties = {
+      width = 1,
+      height = 1,
+      background = data.media.graphics.menu.ggbg
+    }
+  },
+  { kind = 'Element',
+    properties = {
+      y = .2,
+      width = 1,
+      height = .1,
+      background = {0, 0, 0, 80}
+    }
+  }
+}
