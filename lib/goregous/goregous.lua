51b2b318f650f1118a0c9eb05a49e81418c5a053
bjorn <tie.372@gmail.com>
2014-08-31 22:44:11 -0600

    goregous;

--- lib/goregous/goregous.lua
+++ lib/goregous/goregous.lua
@@ -3,7 +3,8 @@ Goregous = class()
 function Goregous:init()
   self.socket = (require('socket')).tcp()
   self.socket:settimeout(10)
-  local ip = (env == 'local') and '127.0.0.1' or '67.0.161.178'
+  local ip = (env == 'local') and '127.0.0.1' or '174.50.68.127'
+	if env == 'sophia' then ip = '10.0.1.28' end
   local _, e = self.socket:connect(ip, 6060)
   if e then error('Can\'t connect to goregous') end
   self.messages = {}
